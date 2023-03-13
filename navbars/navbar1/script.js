bc9fff7692419416f3bb10900695e89e0052238a
RafayMemon01 <rafayrashid200611@gmail.com>
2023-03-13 14:54:48 +0500

    first nav bar was created

---
+++ navbars/navbar1/script.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+r1 = document.querySelector('.navvisi')
+r2 = document.querySelector('.navVisiblilty')
+
+
+
+burger.addEventListener('click',()=>{
+    
+    r1.classList.toggle ('navvisi')
+    r2.classList.toggle  ('navVisiblilty')
+})
